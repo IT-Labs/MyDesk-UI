9ac60ba4219959d10a6df053e47b3a520254813c
manevdusko <manevdusko@gmail.com>
2022-03-02 09:40:10 +0100

    fix form

--- inoffice/src/Pages/Offices/Form.js
+++ inoffice/src/Pages/Offices/Form.js
@@ -1,30 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
+import jwt from "jwt-decode";
+import axios from "axios";
+import { Form, Input, Button } from "antd";
 
-export const Form = ({ onSubmit }) => {
-  return (
-    <form onSubmit={onSubmit}>
-      <div className="form-group">
-        <label htmlFor="name">Office name</label>
-        <input className="form-control" id="name" placeholder="office name" />
+export default class AddOffice extends Component {
+  state = {};
+
+  handleSubmit = (e) => {
+    const data = {
+      name: e.name,
+      location: e.location,
+    };
+
+    console.log(data.name + " - " + data.location);
+
+    // axios
+    //   .post("login", data)
+    //   .then((res) => {
+    //     const user = jwt(res.data);
+    //     localStorage.setItem("token", res.data);
+    //     if (user.role == "ADMIN") {
+    //       window.location = "/admin/dashboard";
+    //     } else if (user.role == "EMPLOYEE") {
+    //       window.location = "/employee/home";
+    //     }
+    //   })
+    //   .catch((err) => {
+    //     this.setState({
+    //       error: "invalid credentials",
+    //     });
+    //   });
+  };
+
+  render() {
+    return (
+      <div>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={this.handleSubmit}
+        >
+          <Form.Item
+            name="name"
+            rules={[{ required: true, message: "Please input office name!" }]}
+          >
+            <Input placeholder="Office name" />
+          </Form.Item>
+          <Form.Item
+            name="location"
+            rules={[
+              { required: true, message: "Please input office location!" },
+            ]}
+          >
+            <Input placeholder="Office location" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="submit" htmlType="submit" className="formButton">
+              Save
+            </Button>
+            <Button
+              className="formButton"
+              onClick={() => (window.location = "/admin/offices")}
+            >
+              Cancel
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
-      <div className="form-group">
-        <label htmlFor="location">Office location</label>
-        <input
-          type="text"
-          className="form-control"
-          id="location"
-          placeholder="location"
-        />
-      </div>
-      <div className="form-group">
-        <button className="formButton" type="submit">
-          Save
-        </button>
-        <button className="formButton" type="submit">
-          Cancel
-        </button>
-      </div>
-    </form>
-  );
-};
-export default Form;
+    );
+  }
+}
