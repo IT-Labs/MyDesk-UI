d484c2d56752e8d08a5aaa79989f32df05090e94
Aleksandar Beleshovski <aleksandar.beleshovski@it-labs.com>
2022-05-11 12:59:07 +0000

    Merged PR 2121: back to prod url
    back to prod url

--- inoffice/src/components/LoginForm/Login.jsx
+++ inoffice/src/components/LoginForm/Login.jsx
@@ -20,9 +20,7 @@ function Login(props) {
     };
 
     const token = localStorage.getItem("msal.idtoken");
-    if (token) {
-      navigate("/admin/dashboard");
-    }
+
     api
       .post("/authentication", info)
       .then((res) => {
@@ -57,7 +55,7 @@ function Login(props) {
           tenantUrl={
             "https://login.microsoftonline.com/{9a433611-0c81-4f7b-abae-891364ddda17}/"
           }
-          redirectUri={"http://localhost:3000"}
+          redirectUri={"https://salmon-grass-030b2a503.1.azurestaticapps.net/"}
           forceRedirectStrategy={true}
           useLocalStorageCache={true}
           withUserData={true}
