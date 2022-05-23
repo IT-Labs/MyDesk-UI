e7a9e86800fb4beaefa58e849eda46da75ea00dd
Aleksandar Beleshovski <aleksandar.beleshovski@it-labs.com>
2022-05-23 11:58:30 +0000

    Merged PR 2198: Added Reservation List
    - added char limit
    - added reservation list

--- inoffice/src/pages/ReservationList/ReservationList.jsx
+++ inoffice/src/pages/ReservationList/ReservationList.jsx
@@ -1,21 +1,109 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Layout, { Content } from "antd/lib/layout/layout";
 import UserHeade from "../../components/Head/UserHead";
-import { Card } from "antd";
+import { Button, Card, Table } from "antd";
+import api from "../../helper/api";
+import styles from "./ReservationList.module.css";
+import { Excel } from "antd-table-saveas-excel";
 
-export default class ReservationList extends Component {
-  render() {
-    return (
-      <Layout>
-        <UserHeade />
-        <Layout>
-          <Sidebar selected="4" />
-          <Content>
-            <Card title="Reservation List"></Card>
-          </Content>
-        </Layout>
-      </Layout>
+const ReservationList = () => {
+  const [reservations, setReservations] = useState([]);
+
+  const sortResStruct = (res) => {
+    console.log(res);
+    const results = res.map(
+      ({ employee, officeName, desk, conferenceRoom }, id) => {
+        return {
+          employee: `${employee.firstName} ${employee.lastName}`,
+          office: officeName ? officeName : "Undefined office",
+          entity: desk ? `Desk [${desk.indexForOffice}]` : "Undefined Desk",
+          key: id,
+        };
+      }
     );
-  }
-}
+    setReservations(results);
+  };
+
+  const getAllRes = () => {
+    api
+      .get("employee/reservations/all")
+      .then(({ data }) => {
+        sortResStruct(data.reservations);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getAllRes();
+  }, []);
+
+  const columns = [
+    { title: "Employee", dataIndex: "employee", key: 1 },
+    { title: "Office", dataIndex: "office", key: 2 },
+    { title: "Entity", dataIndex: "entity", key: 3 },
+  ];
+  return (
+    <Layout>
+      <UserHeade />
+      <Layout>
+        <Sidebar selected="4" />
+        <Content className={styles.content}>
+          <div style={{ width: "80%" }}>
+            <Card
+              title={<Title reservations={reservations} columns={columns} />}
+              className={styles.resList}
+            >
+              <Table
+                columns={columns}
+                dataSource={reservations}
+                pagination={{ pageSize: 5 }}
+                className={styles.table}
+              ></Table>
+            </Card>
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+const Title = ({ reservations, columns }) => {
+  const handleClick = () => {
+    const excel = new Excel();
+    excel
+      .addSheet("test")
+      .addColumns(columns)
+      .addDataSource(reservations, {
+        str2Percent: true,
+      })
+      .saveAs("Excel.xlsx");
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <div className="ant-card-head-title">Reservation List</div>
+      <Button
+        style={{
+          border: "1px solid teal",
+          borderRadius: 7,
+          width: 120,
+          height: 40,
+          color: "teal",
+        }}
+        onClick={handleClick}
+      >
+        Export Data
+      </Button>
+    </div>
+  );
+};
+
+export default ReservationList;
