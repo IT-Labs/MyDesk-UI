379c850ef0cb5a8094b64636e17e82fec7a44a77
Aleksandar Beleshovski <aleksandar.beleshovski@it-labs.com>
2022-07-04 09:58:23 +0000

    Merged PR 2333: found it
    found it

--- inoffice/src/components/Head/UserHead.jsx
+++ inoffice/src/components/Head/UserHead.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./Header.module.scss";
 import { Header } from "antd/lib/layout/layout";
