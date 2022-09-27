8d704663faddc0fa4d18ae4b9a8804cbb3e5c52b
Maria Aguilera <maria.aguilera@it-labs.com>
2022-09-27 18:33:50 -0500

    splitting login tests in 2 files: successful and failed login

---
+++ inoffice/cypress/e2e/login/failed-login.cy.js
@@ -0,0 +1,24 @@
+import { loginPage } from "../../support/pages/login";
+import * as userData from "../../fixtures/userData.json";
+
+describe("Login functionality", () => {
+  const INCORRECT_PASSWORD = "incorrect-password";
+
+  it("Verifies user cannot login with invalid credentials", () => {
+    cy.visit("/");
+    loginPage.doLogin("incorrect-email@test.com", INCORRECT_PASSWORD);
+    loginPage.verifyIncorrectCredentialsMessageIsDisplayed();
+  });
+
+  it("Verifies user cannot login with valid email and invalid password", () => {
+    cy.visit("/");
+    loginPage.doLogin(userData.cypressAutomationUserEmail, INCORRECT_PASSWORD);
+    loginPage.verifyIncorrectCredentialsMessageIsDisplayed();
+  });
+
+  it("Verifies user cannot login empty credentials", () => {
+    cy.visit("/");
+    loginPage.clickSubmitButton();
+    cy.url().should("eq", Cypress.config("baseUrl"));
+  });
+});
--- inoffice/cypress/e2e/login/successful-login.cy.js
+++ inoffice/cypress/e2e/login/successful-login.cy.js
@@ -4,27 +4,7 @@ import * as userData from "../../fixtures/userData.json";
 import * as urlSlugs from "../../fixtures/urlSlugs.json";
 
 describe("Login functionality", () => {
-  const INCORRECT_PASSWORD = "incorrect-password";
-
-  it("Verifies user cannot login with invalid credentials", () => {
-    cy.visit("/");
-    loginPage.doLogin("incorrect-email@test.com", INCORRECT_PASSWORD);
-    loginPage.verifyIncorrectCredentialsMessageIsDisplayed();
-  });
-
-  it("Verifies user cannot login with valid email and invalid password", () => {
-    cy.visit("/");
-    loginPage.doLogin(userData.cypressAutomationUserEmail, INCORRECT_PASSWORD);
-    loginPage.verifyIncorrectCredentialsMessageIsDisplayed();
-  });
-
-  it("Verifies user cannot login empty credentials", () => {
-    cy.visit("/");
-    loginPage.clickSubmitButton();
-    cy.url().should("eq", Cypress.config("baseUrl"));
-  });
-
-  it("Verifies user can login valid credentials", () => {
+  it("Verifies user can login with valid credentials", () => {
     cy.visit("/");
     loginPage.doLogin(
       userData.cypressAutomationUserEmail,
