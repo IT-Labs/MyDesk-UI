beaa681dd14c6c4a7c0b5375eb7c2105941a5d79
Maria Aguilera <maria.aguilera@it-labs.com>
2022-10-07 11:41:03 -0500

    adding a folder for admin reservations tests

---
+++ inoffice/cypress/e2e/admin-reservations/offices.cy.js
@@ -0,0 +1,26 @@
+import { loginPage } from "../../support/pages/login";
+import { homeEmployeePage } from "../../support/pages/home-employee";
+import { adminReservationsPage } from "../../support/pages/admin-reservations";
+
+import * as userData from "../../fixtures/userData.json";
+import * as urlSlugs from "../../fixtures/urlSlugs.json";
+
+describe("Users can select a desk", () => {
+  // Failing due to bug https://dev.azure.com/ITLabs-LLC/Internship%202022/_workitems/edit/53491/
+  // @TODO complete once bug is fixed
+  it.skip("Verify that when the user selects the office, only the reservations made in the selected office are being shown", () => {
+    cy.visit("/");
+    loginPage.doLogin(
+      userData.cypressAutomationUserEmail,
+      userData.genericPassword
+    );
+    cy.url().should("contain", urlSlugs.employee + urlSlugs.home);
+    homeEmployeePage.assertUserNameInHeader(
+      `${userData.cypressAutomationFirstName} ${userData.cypressAutomationLastName}`
+    );
+    cy.assertLoadingDotsNotVisible();
+    cy.visit(urlSlugs.admin + urlSlugs.reservations);
+    cy.assertLoadingDotsNotVisible();
+    adminReservationsPage.assertOfficeIsFiltered();
+  });
+});
--- inoffice/cypress/e2e/home-employee/offices.cy.js
+++ inoffice/cypress/e2e/home-employee/offices.cy.js
@@ -1,8 +1,6 @@
 import { loginPage } from "../../support/pages/login";
 import { homeEmployeePage } from "../../support/pages/home-employee";
 import { employeeReservationsPage } from "../../support/pages/employee-reservations";
-import { adminReservationsPage } from "../../support/pages/admin-reservations";
-
 import * as userData from "../../fixtures/userData.json";
 import * as urlSlugs from "../../fixtures/urlSlugs.json";
 
@@ -32,23 +30,5 @@ describe("Users can select a desk", () => {
     cy.assertLoadingDotsNotVisible();
     cy.visit(urlSlugs.employee + urlSlugs.reservations);
     employeeReservationsPage.assertOfficeIsFiltered();
-  });
-
-  // Failing due to bug https://dev.azure.com/ITLabs-LLC/Internship%202022/_workitems/edit/53491/
-  // @TODO complete once bug is fixed
-  it.skip("Verify that when the user selects the office, only the reservations made in the selected office are being shown", () => {
-    cy.visit("/");
-    loginPage.doLogin(
-      userData.cypressAutomationUserEmail,
-      userData.genericPassword
-    );
-    cy.url().should("contain", urlSlugs.employee + urlSlugs.home);
-    homeEmployeePage.assertUserNameInHeader(
-      `${userData.cypressAutomationFirstName} ${userData.cypressAutomationLastName}`
-    );
-    cy.assertLoadingDotsNotVisible();
-    cy.visit(urlSlugs.admin + urlSlugs.reservations);
-    cy.assertLoadingDotsNotVisible();
-    adminReservationsPage.assertOfficeIsFiltered();
   });
 });
